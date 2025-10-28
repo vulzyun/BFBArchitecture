bbf2ff0372ed1485041fdf11f70dacf691bc3393
vulzyun <matteo.padou@etu.imt-nord-europe.fr>
2025-10-28 13:45:45 +0100

    initialisation

---
+++ demo/src/main/java/com/BFBManagement/demo/BfbManagementApplication.java
@@ -0,0 +1,13 @@
+package com.BFBManagement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfbManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfbManagementApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/BFBManagement/demo/BfbManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BFBManagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfbManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
