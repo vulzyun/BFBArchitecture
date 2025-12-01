dbd876ae8ec934f9b4ec6fa51261953dc49ce11d
vulzyunn <matteo.padou@sage.com>
2025-12-01 11:10:23 +0100

    fix getall, add colonnesbdd for clients, change to postegres

--- demo/src/main/java/com/bfb/interfaces/rest/client/ClientController.java
+++ demo/src/main/java/com/bfb/interfaces/rest/client/ClientController.java
@@ -1,18 +1,28 @@
 package com.bfb.interfaces.rest.client;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.bfb.business.client.model.Client;
 import com.bfb.business.client.service.ClientService;
 import com.bfb.interfaces.rest.client.dto.ClientDto;
 import com.bfb.interfaces.rest.client.dto.CreateClientRequest;
 import com.bfb.interfaces.rest.client.mapper.ClientMapper;
 import com.bfb.interfaces.rest.common.BaseRestController;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 /**
  * REST controller for client management.
@@ -48,15 +58,31 @@ public class ClientController extends BaseRestController<Client, ClientDto> {
     @GetMapping
     @Operation(
         summary = "Get all clients", 
-        description = "Retrieves the list of all clients with pagination support. Use ?page=0&size=20&sort=name,asc for pagination."
+        description = "Retrieves the list of all clients with pagination support"
     )
     public ResponseEntity<org.springframework.data.domain.Page<ClientDto>> getAll(
-        @io.swagger.v3.oas.annotations.Parameter(
-            description = "Pagination parameters (page, size, sort)",
-            example = "page=0&size=20&sort=name,asc"
-        )
-        org.springframework.data.domain.Pageable pageable
+        @RequestParam(defaultValue = "0") 
+        @io.swagger.v3.oas.annotations.Parameter(description = "Page number (0-based)") 
+        int page,
+        
+        @RequestParam(defaultValue = "20") 
+        @io.swagger.v3.oas.annotations.Parameter(description = "Page size") 
+        int size,
+        
+        @RequestParam(defaultValue = "nom,asc") 
+        @io.swagger.v3.oas.annotations.Parameter(description = "Sort criteria (field,direction)") 
+        String sort
     ) {
+        // Parse sort parameter
+        String[] sortParams = sort.split(",");
+        org.springframework.data.domain.Sort.Direction direction = 
+            sortParams.length > 1 && sortParams[1].equalsIgnoreCase("desc") 
+                ? org.springframework.data.domain.Sort.Direction.DESC 
+                : org.springframework.data.domain.Sort.Direction.ASC;
+        
+        org.springframework.data.domain.Pageable pageable = 
+            org.springframework.data.domain.PageRequest.of(page, size, direction, sortParams[0]);
+        
         org.springframework.data.domain.Page<Client> clients = clientService.findAll(pageable);
         org.springframework.data.domain.Page<ClientDto> dtos = clients.map(clientMapper::toDto);
         return okPage(dtos);
