1cbaba2e85b740685b803b42ca388b19df11afca
youstcc <steckoyouri@gmail.com>
2026-01-08 18:01:51 +0100

    cassage main

--- demo/src/main/java/com/bfb/BfbManagementApplication.java
+++ demo/src/main/java/com/bfb/BfbManagementApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BfbManagementApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BfbManagementApplication.class, args);
+		SpringApplication.run(BfbManagementApplication.class, args);
 	}
 
 }
